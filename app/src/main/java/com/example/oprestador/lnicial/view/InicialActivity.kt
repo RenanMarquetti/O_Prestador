8272d6ea4b55c4c3f8a06f3b493dc1262ef62d27
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2023-06-16 07:17:08 -0300

    coloquei binding na classe InicialActivity

--- app/src/main/java/com/example/oprestador/lnicial/view/InicialActivity.kt
+++ app/src/main/java/com/example/oprestador/lnicial/view/InicialActivity.kt
@@ -6,16 +6,21 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.oprestador.R
+import com.example.oprestador.databinding.ActivityInicialBinding
 
 
 class InicialActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityInicialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityInicialBinding.inflate(layoutInflater)
+
         installSplashScreen()
 
-        setContentView(R.layout.activity_inicial)
+        setContentView(binding.root)
 
         val navInicialController = supportFragmentManager.findFragmentById(R.id.nav_inicial_fragment) as NavHostFragment
         val navController : NavController = navInicialController.navController
