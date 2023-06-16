272153faf72efdbce9429b38189c3197b4160784
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2023-06-15 21:48:24 -0300

    inseri as otimizações do binding no na classe ClienteActivity.kt

--- app/src/main/java/com/example/oprestador/cliente/view/ClienteActivity.kt
+++ app/src/main/java/com/example/oprestador/cliente/view/ClienteActivity.kt
@@ -4,43 +4,45 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.oprestador.R
+import com.example.oprestador.databinding.ActivityClienteBinding
 import com.google.android.material.navigation.NavigationView
 
 class ClienteActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var binding: ActivityClienteBinding
+    private lateinit var navControler: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_cliente)
+        binding = ActivityClienteBinding.inflate(layoutInflater)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar_cliente)
-        setSupportActionBar(toolbar)
+        setContentView(binding.root)
 
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout_cliente)
-        val navView = findViewById<NavigationView>(R.id.nav_view_cliente)
+        setSupportActionBar(binding.appBarCliente.toolbarCliente)
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_fragmentPedidoNovo, R.id.nav_fragmentPedidosAbertos
-            ),drawerLayout
+            ),binding.drawerLayoutCliente
         )
 
-        val navControler = findNavController(R.id.nav_cliente_fragment)
+        navControler = findNavController(R.id.nav_cliente_fragment)
 
         setupActionBarWithNavController(navControler,appBarConfiguration)
-        navView.setupWithNavController(navControler)
+        binding.navViewCliente.setupWithNavController(navControler)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navControler = findNavController(R.id.nav_cliente_fragment)
         return navControler.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
