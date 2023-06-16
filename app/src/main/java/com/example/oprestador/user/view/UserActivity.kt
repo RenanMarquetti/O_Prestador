18ba73977e30f0cb929df0fb3bb0bea4a9acb850
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2023-06-16 08:14:09 -0300

    coloquei binding na classe UserActivity

--- app/src/main/java/com/example/oprestador/user/view/UserActivity.kt
+++ app/src/main/java/com/example/oprestador/user/view/UserActivity.kt
@@ -4,47 +4,51 @@ package com.example.oprestador.user.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.oprestador.R
+import com.example.oprestador.databinding.ActivityUserBinding
 import com.google.android.material.navigation.NavigationView
 
 class UserActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var binding: ActivityUserBinding
+    private lateinit var navControler: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        binding
 
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val navView = findViewById<NavigationView>(R.id.nav_view)
+        with(binding) {
 
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_fragmentListaPedidos, R.id.nav_fragmentMeusPedidos
-                , R.id.nav_fragmentPerfil
-            ),drawerLayout
-        )
+            setContentView(root)
 
-        val navControler = findNavController(R.id.nav_user_fragment)
+            setSupportActionBar(appBarMain.toolbar)
 
-        setupActionBarWithNavController(navControler,appBarConfiguration)
-        navView.setupWithNavController(navControler)
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.nav_fragmentListaPedidos, R.id.nav_fragmentMeusPedidos
+                    , R.id.nav_fragmentPerfil
+                ),drawerLayoutUser
+            )
+
+            navControler = findNavController(R.id.nav_user_fragment)
+
+            setupActionBarWithNavController(navControler,appBarConfiguration)
+            navViewUser.setupWithNavController(navControler)
+        }
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navControler = findNavController(R.id.nav_user_fragment)
         return navControler.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
