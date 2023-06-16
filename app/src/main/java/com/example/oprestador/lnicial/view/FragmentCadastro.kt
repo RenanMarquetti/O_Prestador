cdbdb5dc77af62f5c39eb4b5a7f22994c8e55f05
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2023-06-16 08:03:37 -0300

    coloquei binding na classe FragmentCadastro

--- app/src/main/java/com/example/oprestador/lnicial/view/FragmentCadastro.kt
+++ app/src/main/java/com/example/oprestador/lnicial/view/FragmentCadastro.kt
@@ -8,17 +8,29 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.oprestador.R
+import com.example.oprestador.databinding.FragmentCadastroBinding
 
-class FragmentCadastro : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreate(savedInstanceState)
+class FragmentCadastro : Fragment(R.layout.fragment_cadastro) {
 
-        val view = inflater.inflate(R.layout.fragment_cadastro, container, false)
+    private var binding: FragmentCadastroBinding? = null
 
-        view.findViewById<Button>(R.id.btn_cadastro).setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_fragmentCadastro_to_fragmentDivisor)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentCadastroBinding.bind(view)
+
+        with(binding!!) {
+
+            btnCadastro.setOnClickListener {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentCadastro_to_fragmentDivisor)
+            }
+
         }
-        return view
 
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
