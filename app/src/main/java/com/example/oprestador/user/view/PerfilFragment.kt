bf0a854841fed681d123f1a125124618d56bad95
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2023-06-16 21:12:04 -0300

    coloquei binding na classe PerfilFragment

--- app/src/main/java/com/example/oprestador/user/view/PerfilFragment.kt
+++ app/src/main/java/com/example/oprestador/user/view/PerfilFragment.kt
@@ -6,10 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.oprestador.R
+import com.example.oprestador.databinding.FragmentPerfilBinding
 
-class PerfilFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+class PerfilFragment : Fragment(R.layout.fragment_perfil) {
 
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+    private var binding: FragmentPerfilBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentPerfilBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
