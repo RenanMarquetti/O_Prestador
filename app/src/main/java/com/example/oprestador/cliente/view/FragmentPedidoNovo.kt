e75deabe82868b1de2d3e86f77467607d7be797d
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2023-07-02 23:41:09 -0300

    setei um adapter de um array de lista de categoria no FragmentPedidoNovo

--- app/src/main/java/com/example/oprestador/cliente/view/FragmentPedidoNovo.kt
+++ app/src/main/java/com/example/oprestador/cliente/view/FragmentPedidoNovo.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.oprestador.R
 import com.example.oprestador.databinding.FragmentPedidoNovoBinding
@@ -15,6 +16,11 @@ class FragmentPedidoNovo : Fragment(R.layout.fragment_pedido_novo) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPedidoNovoBinding.bind(view)
+
+        val provincias = resources.getStringArray(R.array.categorias)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, provincias)
+        binding!!.pedidoNovoInputCategoria.setAdapter(adapter)
+
     }
 
     override fun onDestroy() {
